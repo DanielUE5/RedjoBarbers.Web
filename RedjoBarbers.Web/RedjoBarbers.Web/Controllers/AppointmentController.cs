0625dc291ac17c1a4bc39e8921e6a4e495ac5564
Daniel <dani94@mail.bg>
2026-02-04 21:49:25 +0200

    Logo added. All controllers were added and tested. Views added. _Layout changed for now. _UserMenuPartial is added for adjusting the view and testing (Authentication type is still not added)

---
+++ RedjoBarbers.Web/RedjoBarbers.Web/Controllers/AppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedjoBarbers.Web.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Works!");
+        }
+
+        public IActionResult Create()
+        {
+            return Ok("Works!");
+        }
+
+        public IActionResult Delete()
+        {
+            return Ok("Works!");
+        }
+
+        public IActionResult Update()
+        {
+            return Ok("Works!");
+        }
+
+        public IActionResult MyAppointments()
+        {
+            return Ok("Works!");
+        }
+    }
+}
