34bdab8086c7aa3e33d3e1482da9397bba324b5c
Daniel <ivanovdaniel51@gmail.com>
2026-04-01 16:55:42 +0300

    Add review filtering and sorting with a page view model. Refactored reviews index to separate page and item models and allow filtering by service and sorting by newest or highest rated.

---
+++ RedjoBarbers.Web/RedjoBarbers.Web/ViewModels/ReviewIndexPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RedjoBarbers.Web.ViewModels
+{
+    public class ReviewIndexViewModel
+    {
+        public int? BarberServiceId { get; set; }
+
+        public string? SortReviews { get; set; }
+
+        public IEnumerable<SelectListItem> Services { get; set; } = new List<SelectListItem>();
+
+        public IEnumerable<ReviewIndexItemViewModel> Reviews { get; set; } = new List<ReviewIndexItemViewModel>();
+    }
+}
